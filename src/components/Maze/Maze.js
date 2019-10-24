dcece49e603d863e0178bb570d346edf892f9d4e
jurusteve <jstevemu17@gmail.com>
2019-10-24 14:54:17 -0400

    displayed map and current room on map

--- src/components/Maze/Maze.js
+++ src/components/Maze/Maze.js
@@ -1,60 +1,135 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
-import {axiosWithAuth} from '../../utils/AxiosWithAuth'
+import Sidebar from "../Sidebar/Sidebar";
+import Styled from "styled-components";
+
+const token = localStorage.getItem("token");
+axios.defaults.headers.common["Authorization"] = `Token ${token}`;
 
 function Maze() {
-    const [details, setDetails] = useState({}) 
-    const [rooms, setRooms] = useState([])
-    const [error, setError] = useState('')
-    const [currentRoom, setCurrentRoom] = useState({})
-    useEffect(() => {
-        axios
-            .get('https://bw-django-game.herokuapp.com/api/adv/init')
-            .then(res => {
-                console.log("init", res);
-                setDetails(res.data);
-                setCurrentRoom(res.data.title, res.data.description);
-                console.log(details);
-            });
-    }, []);
+  const [details, setDetails] = useState({});
+  const [rooms, setRooms] = useState([]);
+  const [error, setError] = useState("");
+  const [loaded, setLoaded] = useState(false);
+  const [currentRoom, setCurrentRoom] = useState({});
 
-    useEffect(() => {
-        axios
-            .get("https://bw-django-game.herokuapp.com/api/adv/rooms")
-            .then(res => {
-                console.log("init", res);
-                setRooms(res.data);
-                console.log(details);
-            });
-    }, []);
+  useEffect(() => {
+    axios.get("https://bw-django-game.herokuapp.com/api/adv/init").then(res => {
+      setDetails(res.data);
+      setCurrentRoom({
+        title: res.data.title,
+        description: res.data.description
+      });
+    });
+  }, []);
 
-    const move = (direction) => {
-        let explore = { 'direction': direction }
-        axios
-            .post('https://bw-django-game.herokuapp.com/api/adv/move', explore)
-            .then(res => {
-                if(res.data.error_msg){
-                    setError(res.data.error_msg)
-                }else{
-                    setError('')
-                }
-                console.log("Move", res)
-                setDetails({title: res.data.title, description:res.data.description})
-            })
-    }
-    return (
-        <>
-        <div>
-            <p>Room Title:<strong>{details.title}</strong></p>
-            <p>Room description:<strong>{details.description}</strong></p>
+  useEffect(() => {
+    axios
+      .get("https://bw-django-game.herokuapp.com/api/adv/rooms")
+      .then(res => {
+        setRooms(res.data.rooms);
+        setLoaded(true);
+      });
+  }, []);
+
+  const move = direction => {
+    let explore = { direction: direction };
+    axios
+      .post("https://bw-django-game.herokuapp.com/api/adv/move", explore)
+      .then(res => {
+        if (res.data.error_msg) {
+          setError(res.data.error_msg);
+        } else {
+          setError("");
+        }
+        console.log("Move", res);
+        setCurrentRoom({
+          title: res.data.title,
+          description: res.data.description
+        });
+        setDetails(res.data);
+      });
+  };
+  return (
+    <MapContainer>
+      <div className="map-container">
+        <p>Map</p>
+        <div className="map">
+          {rooms
+            ? rooms.map((rm, i) => {
+                return (
+                  <div
+                    className={`room-cell ${
+                      // @ts-ignore
+                      rm.title === currentRoom.title ? "current-room" : ""
+                    }`}
+                    key={i}
+                  >
+                    {/* {rm.id} */}
+                  </div>
+                );
+              })
+            : ""}
         </div>
-        <button onClick={() => move('w')}>West</button>
-        <button onClick={() => move('n')}>North</button>
-        <button onClick={() => move('s')}>South</button>
-        <button onClick={() => move('e')}>East</button> 
+        <div className="controls">
+          <button onClick={() => move("w")}>West</button>
+          <button onClick={() => move("n")}>North</button>
+          <button onClick={() => move("s")}>South</button>
+          <button onClick={() => move("e")}>East</button>
+        </div>
+        <div className="errors">{error ? <span>{error}</span> : ""}</div>
+      </div>
 
-        {error ? <span>{error}</span> : ''}
-        </>
-    );
+      <div className="sidebar">
+        <Sidebar move={move} currRoom={currentRoom} />
+      </div>
+    </MapContainer>
+  );
 }
+
+const MapContainer = Styled.div`
+    display:flex;
+    flex-direction: column;
+    width: 100%
+    .map-container{
+        width: 100%;
+        text-align: center;
+        .controls{
+            button {
+                font-size: 20px;
+                padding: 10px;
+                margin: 5px;
+                border-radius: 10px
+                background-color: lightyellow
+            }
+            button:hover{
+                color: lightyellow;
+                background-color: black
+            }
+        }
+        .map{
+            height: 100%;
+            background-color: #515151;
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-evenly;
+            .room-cell{
+                font-size: 15px;
+                background-color: yellow;
+                padding:10px;
+                margin:10px;
+                width: 5px;
+                height: 5px
+            }
+            .current-room{
+                background-color: pink
+            }
+        }
+    }
+    .sidebar{
+        width: 60%
+    }
+
+`;
+
 export default Maze;
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,25 +1,21 @@
 import React from "react";
 
 const SideMenu = props => {
-  console.log(props);
-  const players = props.details.players;
+  const players = props.currRoom.players;
 
-  console.log(players);
-
-  if (props.details) {
+  if (props.currRoom) {
     return (
       <div className="side-menu">
         <div className="player-stats">
-          <h3>{props.details.title}</h3>
-          <p>{props.details.description}</p>
+          <h3>Title: {props.currRoom.title}</h3>
+          <p>Description: {props.currRoom.description}</p>
         </div>
         <div className="players">
-          <h3>Players Here:</h3>
+          <h3>Players:</h3>
           {players
             ? players.map((player, i) => <p key={i}>{player}</p>)
-            : "No players currently here"}
+            : ":-( No online players!"}
         </div>
-        <div className="chat">bottom menu</div>
       </div>
     );
   } else {
